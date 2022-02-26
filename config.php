be412adff18773427074a78aa11b3d6ba0ab9ace
snioer <45099729+snioer@users.noreply.github.com>
2022-02-26 20:58:59 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 return array(
     'redis' => array(
-        'host'  => getenv('DB_PORT_6379_TCP_ADDR')?getenv('DB_PORT_6379_TCP_ADDR'):'localhost',
-        'port'  => getenv('DB_PORT_6379_TCP_PORT')?getenv('DB_PORT_6379_TCP_PORT'):'6379',
+        'host'  => getenv('DB_PORT_6379_TCP_ADDR')?getenv('DB_PORT_6379_TCP_ADDR'):'ec2-44-194-37-188.compute-1.amazonaws.com',
+        'port'  => getenv('DB_PORT_6379_TCP_PORT')?getenv('DB_PORT_6379_TCP_PORT'):'20430',
         'db'    => getenv('DB_NAME')?getenv('DB_NAME'):'',
-        'pass'  => getenv('REDIS_PASSWORD')?getenv('REDIS_PASSWORD'):''
+        'pass'  => getenv('REDIS_PASSWORD')?getenv('REDIS_PASSWORD'):'p7be6a71ce079016356ede575ae45507aa034351c286c2e22426f6915a72de472'
     )
 );
